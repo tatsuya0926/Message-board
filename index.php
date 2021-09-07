322f1e4244f87ba745e263d798b19a58a0815c7b
tatsuya <myuthree1478@gmail.com>
2021-09-08 00:34:27 +0900

    変更３

--- index.php
+++ index.php
@@ -15,10 +15,11 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
 
 if(!empty($_POST)){
   if($_POST['message'] !== ''){
-    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created=NOW()');
     $message->execute(array(
       $member['id'],
-      $_POST['message']
+      $_POST['message'],
+      $_POST['reply_post_id']
     ));
 
     header('Location: index.php');
@@ -27,6 +28,14 @@ if(!empty($_POST)){
 }
 
 $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+if(isset($_REQUEST['res'])){
+  $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+  $response->execute(array($_REQUEST['res']));
+
+  $table = $response->fetch();
+  $message = '@' . $table['name'] . '' . $table['message'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -51,8 +60,8 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
       <dl>
         <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES)); ?>" />
         </dd>
       </dl>
       <div>
@@ -62,23 +71,28 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
       </div>
     </form>
 
-<?php foreach($posts as $post): ?>
+  <?php foreach($posts as $post): ?>
     <div class="msg">
     <img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
     <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name"> By <?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?></span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]</p>
-    <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
-<a href="view.php?id=">
-返信元のメッセージ</a>
-[<a href="delete.php?id="
-style="color: #F33;">削除</a>]
+    <p class="day"><a href="view.php?id=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+
+    <?php if($post['reply_message_id'] > 0): ?>
+      <a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id'], ENT_QUOTES)); ?>">返信元のメッセージ</a>
+    <?php endif; ?>
+
+    <?php if($_SESSION['id'] == $post['member_id']): ?>
+      [<a href="delete.php?id=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>"style="color: #F33;">削除</a>]
+    <?php endif; ?>
+    
     </p>
     </div>
-<?php endforeach; ?>
+  <?php endforeach; ?>
 
-<ul class="paging">
-<li><a href="index.php?page=">前のページへ</a></li>
-<li><a href="index.php?page=">次のページへ</a></li>
-</ul>
+  <ul class="paging">
+  <li><a href="index.php?page=">前のページへ</a></li>
+  <li><a href="index.php?page=">次のページへ</a></li>
+  </ul>
   </div>
 </div>
 </body>
